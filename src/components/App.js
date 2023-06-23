68604a26ac101ff4e8638457fd8f0de3fcb41a8c
TarunVanshkar <tarunben33@gmail.com>
2023-06-23 23:30:09 +0530

    submitting code-Fri Jun 23 2023 23:30:05 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,35 @@
-
-import React from "react";
-import './../styles/App.css';
+import React, { useState, useEffect } from "react";
 
 const App = () => {
+  const suggestions = ["Bhopal", "Indore", "Jabalpur", "Itarsi", "Patna", "Gwalior", "Dhanbad", "Jhasi", "Ahemdabad", "Banglore", "Chennai"];
+  const [query, setQuery] = useState("");
+  const [filteredSuggestions, setFilteredSuggestions] = useState([]);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setFilteredSuggestions(
+        suggestions.filter((suggestion) =>
+          suggestion.toLowerCase().includes(query.toLowerCase())
+        )
+      );
+    }, 500);
+  }, [query, suggestions]);
+
+  const handleInputChange = (event) => {
+    setQuery(event.target.value);
+  };
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      <h1>Search item</h1>
+      <input type="text" value={query} onChange={handleInputChange} />
+      <ul>
+        {filteredSuggestions.map((suggestion) => (
+          <li key={suggestion}>{suggestion}</li>
+        ))}
+      </ul>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
